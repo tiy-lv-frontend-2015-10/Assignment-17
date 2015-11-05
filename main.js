e2e9b7d7101878336d32d1b66757c009485d754f
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-05 15:21:15 -0800

    added array function exception

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 
 	var playerArr=[];
 	var simonArr=[];
+	
+	
+	
+	
 	function randomRange(min,max) {
 		return Math.floor(Math.random()*4 +1);
 	};
@@ -35,6 +39,21 @@ $(document).ready(function(){
 
 	};
 
+	function checker(array1,array2) {
+		var len1=array1.length;
+		var arr2 = array2.slice(0, len1);
+		if (arraysEqual(array1, arr2)){
+			
+			console.log(true);
+	} else {
+		console.log(false);
+		
+		$("#start").hide();
+		$("#playAgain").show();
+	}
+}
+
+
 
 	
 	$("#start").on('click',function(){
@@ -61,45 +80,60 @@ $(document).ready(function(){
 
 	$("#red").on('click',function(){
 		playerArr.push(1);
-		comparison(playerArr,simonArr);
+		checker(playerArr,simonArr);
 		simonArr.push(randomRange(1,4));
 		console.log(playerArr);
+		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
 	
 	});
 	$("#blue").on('click',function(){
 		playerArr.push(2);
-		comparison(playerArr,simonArr);
+			checker(playerArr,simonArr);
 		simonArr.push(randomRange(1,4));
 		console.log(playerArr);
+		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
 	
 	});
 	$("#yellow").on('click',function(){
 		playerArr.push(3);
-		comparison(playerArr,simonArr);
+			checker(playerArr,simonArr);
 		simonArr.push(randomRange(1,4));
 		console.log(playerArr);
+		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
 	
 	});
 	$("#green").on('click',function(){
 		playerArr.push(4);
-		comparison(playerArr,simonArr);
+			checker(playerArr,simonArr);
 		simonArr.push(randomRange(1,4));
 		console.log(playerArr);
+		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
 	
 	});
-
 });
 	
 
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
 
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
 	
 	
 
