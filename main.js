ba7e4dc87ed7e2e353ed0da5eea8a6dfb007e284
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-05 09:02:24 -0800

    added comparison

--- main.js
+++ main.js
@@ -6,69 +6,88 @@ $(document).ready(function(){
 	function randomRange(min,max) {
 		return Math.floor(Math.random()*4 +1);
 	};
+
+	function comparison(playerArr,simonArr) {
+		if(playerArr.length!==simonArr.length) return false;{
+			for(var i=0; i<simonArr.length; i++)
+				if(playerArr[i]!==simonArr[i]) {
+					return false;
+				} else {
+					return true;
+				}
+		}
+	}
 	
 	$("#start").on('click',function(){
 		
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);
 
-	})
+	});
 
 	$("#red").on('click',function(){
 		playerArr.push(1);
+		console.log(playerArr);
+		comparison(playerArr,simonArr);
 	
 	});
 	$("#blue").on('click',function(){
 		playerArr.push(2);
+		console.log(playerArr);
 	
 	});
 	$("#yellow").on('click',function(){
 		playerArr.push(3);
+		console.log(playerArr);
 		
 	});
 	$("#green").on('click',function(){
 		playerArr.push(4);
+		console.log(playerArr);
 		
 	});
-		if(playerArr===simonArr) {
+	// 	if(playerArr.length===simonArr.length) {
 
+	// 	if(playerArr.length&&simonArr.length===1) {
+	// 	simonArr.push(randomRange(1,4));
+	// }
 	$("#red").on('click',function(){
-		if(playerArr===simonArr) {
+		
 		$("#playerDiv").html(playerArr);
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);	
 		console.log(playerArr);
-	}
+	
 	});
 	$("#blue").on('click',function(){
-		if(playerArr===simonArr) {
+		
 		$("#playerDiv").html(playerArr);
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);
 		console.log(playerArr);
-	}
+	
 	});
 	$("#yellow").on('click',function(){
-		if(playerArr===simonArr) {
+		
 
 
 		$("#playerDiv").html(playerArr);
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);
 		console.log(playerArr);
-	}
+
 	});
 	$("#green").on('click',function(){
-			if(playerArr===simonArr){
+			
 		$("#playerDiv").html(playerArr);
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);
 		console.log(playerArr);
-	}
+	
 	});
 
+// }
 
-}
 });
 	
 	
