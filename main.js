e3cbfee8b59d8b96bf9f2178f423f4a96e125dfd
Kristie White <insanelynocturnal@gmail.com>
2015-11-05 13:41:41 -0800

    attack options

--- main.js
+++ main.js
@@ -1,35 +1,9 @@
-!function() {
 $(document).ready(function () {
 
-  var Player = function () {
-      this.name;
-      this.health = 1000;
-      this.attack = function (opponent) {
-          var attackChance = Math.random();
-          if (attackChance) > .9) {
-              opponent.health -= 10;
-          } else {
-              console.log(this.name + " missed!!!!");
-          }
-      };
-      this.criticalHit = function (opponent) {
-          opponent.health -= this.randomCris();
-      };
-      this.randomCrit = function () {
-          var max = 18;
-          var min = 7;
-          
-          return Math.floor(Math.random() * (max-min) + min);
-      }
-  };
-    
-    var p1 = new Player();
-    var p2 = new Player();
-    
-    p1.name = "Peter";
-    p2.name = "Chicken";
+  function Players() {
+      this.
+  }
     
     
     
 });//closes ready
-}();//closes iffe
