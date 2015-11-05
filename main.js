3a89c2a3c503dff9756bf5da071a6f208561a395
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-04 17:42:23 -0800

    added comparisons not correct

--- main.js
+++ main.js
@@ -8,37 +8,67 @@ $(document).ready(function(){
 	};
 	
 	$("#start").on('click',function(){
+		
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);
+
 	})
 
 	$("#red").on('click',function(){
 		playerArr.push(1);
-		$("#playerDiv").html(playerArr);
-		simonArr.push(randomRange(1,4));
-		$("#simonDiv").html(simonArr);
+	
 	});
 	$("#blue").on('click',function(){
 		playerArr.push(2);
-		$("#playerDiv").html(playerArr);
-		simonArr.push(randomRange(1,4));
-		$("#simonDiv").html(simonArr);
+	
 	});
 	$("#yellow").on('click',function(){
 		playerArr.push(3);
-		$("#playerDiv").html(playerArr);
-		simonArr.push(randomRange(1,4));
-		$("#simonDiv").html(simonArr);
+		
 	});
 	$("#green").on('click',function(){
 		playerArr.push(4);
+		
+	});
+		if(playerArr===simonArr) {
+
+	$("#red").on('click',function(){
+		if(playerArr===simonArr) {
+		$("#playerDiv").html(playerArr);
+		simonArr.push(randomRange(1,4));
+		$("#simonDiv").html(simonArr);	
+		console.log(playerArr);
+	}
+	});
+	$("#blue").on('click',function(){
+		if(playerArr===simonArr) {
 		$("#playerDiv").html(playerArr);
 		simonArr.push(randomRange(1,4));
 		$("#simonDiv").html(simonArr);
+		console.log(playerArr);
+	}
+	});
+	$("#yellow").on('click',function(){
+		if(playerArr===simonArr) {
+
+
+		$("#playerDiv").html(playerArr);
+		simonArr.push(randomRange(1,4));
+		$("#simonDiv").html(simonArr);
+		console.log(playerArr);
+	}
+	});
+	$("#green").on('click',function(){
+			if(playerArr===simonArr){
+		$("#playerDiv").html(playerArr);
+		simonArr.push(randomRange(1,4));
+		$("#simonDiv").html(simonArr);
+		console.log(playerArr);
+	}
 	});
 
 
-
+}
 });
 	
 	
