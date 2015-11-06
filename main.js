246fc6ecc9e5c7d946937cf94cba5e02ab3686f8
notewrangler <noiseartist@gmail.com>
2015-11-05 16:46:11 -0800

    first draft of problems.js & solutions.js done, html started, js started

---
+++ main.js
@@ -0,0 +1,17 @@
+function Player(name, gender){
+
+	this.name = name;
+	this.gender = gender;
+	this.score;
+}
+
+function Game() {
+
+	this.turnCount = 0;
+
+}
+
+
+var player1 = new Player();
+
+var thisGame = new Game();
