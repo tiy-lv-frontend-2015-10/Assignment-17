6332f288b7a78381edbe2b4d28f1c14c6fda9147
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-05 17:02:26 -0800

    moved push to just checker

--- main.js
+++ main.js
@@ -18,40 +18,50 @@ $(document).ready(function(){
 		$("#simonDiv").html("");
 	}
 
-	function playerReset() {
-		playerArr=[];
-		$("#playerDiv").html("");
-	}
+	
 
-	function comparison(playerArr,simonArr) {
+	// function comparison(playerArr,simonArr) {
 
-			for(var i=0; i<simonArr.length; i++)
-				if(playerArr[i]===simonArr[i]) {
-					console.log(true);
-					
+	// 		for(var i=0; i<simonArr.length; i++)
+	// 			if(playerArr[i]===simonArr[i]) {
+	// 				console.log(true);
+
+	// 	} else {
+	// 		console.log(false);
+	// 		reset();
+	// 		$("#start").hide();
+	// 		$("#playAgain").show();
+
+	// }
+
+	// };
+
+	function playerReset(array1,array2) {
+		var len1=array1.length;
+		var arr2 = array2.slice(0, len1);
+		if (len1===arr2) {
+			console.log(array1===array2.length)
+			array1=[];
+			$("#playerDiv").html("");
+
+		} 
+}	
+	
+	function checker(array1,array2) {
+		var len1=array1.length;
+		var arr2 = array2.slice(0, len1);
+		if (arraysEqual(array1, arr2)){
+			console.log(true);
+			simonArr.push(randomRange(1,4));
+			$("#playerDiv").html(playerArr);
+			console.log(array1);
 		} else {
 			console.log(false);
 			reset();
 			$("#start").hide();
 			$("#playAgain").show();
-
+		}
 	}
-
-	};
-
-	function checker(array1,array2) {
-		var len1=array1.length;
-		var arr2 = array2.slice(0, len1);
-		if (arraysEqual(array1, arr2)){
-			
-			console.log(true);
-	} else {
-		console.log(false);
-		
-		$("#start").hide();
-		$("#playAgain").show();
-	}
-}
 
 
 
@@ -81,67 +91,77 @@ $(document).ready(function(){
 	$("#red").on('click',function(){
 		playerArr.push(1);
 		checker(playerArr,simonArr);
-		simonArr.push(randomRange(1,4));
+	
+		playerReset(playerArr,simonArr);
 		console.log(playerArr);
 		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
-	
+
 	});
 	$("#blue").on('click',function(){
 		playerArr.push(2);
-			checker(playerArr,simonArr);
-		simonArr.push(randomRange(1,4));
+		checker(playerArr,simonArr);
+
+		playerReset(playerArr,simonArr);
 		console.log(playerArr);
 		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
-	
+
+
 	});
 	$("#yellow").on('click',function(){
 		playerArr.push(3);
-			checker(playerArr,simonArr);
-		simonArr.push(randomRange(1,4));
+		checker(playerArr,simonArr);
+
+		playerReset(playerArr,simonArr);
 		console.log(playerArr);
 		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
-	
+
+
 	});
 	$("#green").on('click',function(){
 		playerArr.push(4);
-			checker(playerArr,simonArr);
-		simonArr.push(randomRange(1,4));
+		checker(playerArr,simonArr);
+
+		playerReset(playerArr,simonArr);
 		console.log(playerArr);
 		console.log(simonArr);
 		$("#playerDiv").html(playerArr);
 		$("#simonDiv").html(simonArr);
-	
+
+
 	});
-});
+
 	
 
-function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
+	function arraysEqual(a, b) {
+		if (a === b) return true;
+		if (a == null || b == null) return false;
+		if (a.length != b.length) return false;
 
   // If you don't care about the order of the elements inside
   // the array, you should sort both arrays here.
 
   for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
+  	if (a[i] !== b[i]) return false;
   }
   return true;
 }
-	
-	
 
 
 
-	
-	
-	
 
-	
-	
+});
+
+
+
+
+
+
+
+
+
