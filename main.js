b9cf35b4e3b007a50a5f5c3c528b2be200b4333b
Kristie White <insanelynocturnal@gmail.com>
2015-11-05 16:54:27 -0800

    js in works

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
 $(document).ready(function () {
 
-  function Players() {
-      this.
-  }
+function Player() {
+    this.name;
+    this.health= 50; 
+    this.attack = function(opponent) {
+        opponent.healh -= randomNum;
+    }
+var randomNum = Math.floor((Math.random() * 10) + 1);
+};
     
+var p1 = new Player();
+var p2 = new Player();
     
+p1.name = "Peter";
+p2.name = "Chicken";
+p1.health = "";
+p2.health = ;
+p1.attack = "";
+p2.attack = ;
+    
+Player.prototype.attack = function(player) {
+    
+}
+    
+p1.attack(p2);
+    Player.prototype.attack = function(player) {
+        player.health -= randomNum;
+    };
+p2.attack(p1);
+    Player.prototype.attack = function(player) {
+        player.health -= randomNum;
+    };
+     
+function PlayVideo() {
+    this.load();
+    this.play = function() {
+        
+    }
+}    
+    
+var punchesChicken = new PlayVideo();
+var throwsChicken = new PlayVideo();
+var clubsChicken = new PlayVideo();
+var stranglesChicken = new PlayVideo();
+var electrocutesPeter = new PlayVideo();
+var crushPetersHead = new PlayVideo();
+var kickPetersFace = new PlayVideo();
+var punchesPeter = new PlayVideo();
+    
+play.punches
+
+    
+//add video on click
+//$("#video").on()
+
     
 });//closes ready
