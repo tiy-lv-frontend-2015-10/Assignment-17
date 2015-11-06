744053328033d95287275c03fff5a2844c6d37ec
Kristie White <insanelynocturnal@gmail.com>
2015-11-05 17:37:44 -0800

    may install youtube api

--- main.js
+++ main.js
@@ -33,11 +33,11 @@ p2.attack(p1);
     };
      
 function PlayVideo() {
-    this.load();
+    this.load();//add youtube video link
     this.play = function() {
         
     }
-}    
+};    
     
 var punchesChicken = new PlayVideo();
 var throwsChicken = new PlayVideo();
@@ -48,11 +48,25 @@ var crushPetersHead = new PlayVideo();
 var kickPetersFace = new PlayVideo();
 var punchesPeter = new PlayVideo();
     
-play.punches
+var peterDies = new PlayVideo();
+var chickenDies = new PlayVideo();
+    
+punchesChicken.play = ;//start time & duration 2:48
+throwsChicken.play = ;//start time 2:55
+clubsChicken.play = ;//start time 3:04
+stranglesChicken.play = ;//start time 4:03
+electrocutesPeter.play = ;//start time 2:12
+crushPetersHead.play = ;//start time 4:15
+kickPetersFace.play = ;//start time 3:59
+punchesPeter.play = ;//start time 4:15
+
+peterDies.play = ;//start time 4:52
+chickenDies.play = ;//start time 4:36
+
 
     
 //add video on click
-//$("#video").on()
+$("#video").on("click", function() 
 
     
 });//closes ready
