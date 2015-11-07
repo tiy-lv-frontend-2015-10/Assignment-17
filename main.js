ce22d2453b97417462fe931407ce281fd36a32e5
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-11-07 14:11:28 -0800

    messing with js code

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
 
-function Player(name, health, punch, kick, specialtyHit, bite) {
+function Player() {
   	this.name;
   	this.health = 150;
   	this.punch = function (opponent) {
-    var hitAccuracy = Math.random();
-    if (hitAccuracy > .5) {
-      opponent.health -= 10;
-    } else {
-      alert("Seriously " + p2.name + "! Stop moving!");
-    }
+	    var hitAccuracy = Math.random();
+	    if (hitAccuracy > .5) {
+	      opponent.health -= 10;
+	    } else {
+	      alert("Seriously " + opponent.name + "! Stop moving!");
+	    }
   };
     this.kick = function (opponent) {
-    var hitAccuracy = Math.random();
-    if (hitAccuracy > .3) {
-      opponent.health -= 15;
-    } else {
-      alert("DANGIT!");
-    }
+	    var hitAccuracy = Math.random();
+	    if (hitAccuracy > .5) {
+	      opponent.health -= 10;
+	    } else {
+	      alert("DANGIT!");
+	    }
   };
   	this.specialtyHit = function (opponent) {
-    opponent.health -= this.randomCrit();
+    opponent.health -= this.critDamage();
   };
-  	this.randomCrit = function () {
-    var max = 30;
-    var min = 15;
-    return Math.ceil(Math.random() * 10 + min);
-  }
+  	this.critDamage = function () {
+	    var max = 30;
+	    var min = 15;
+	    return Math.ceil(Math.random() * (max - min) + min);
+	  }
 };
 	var p1 = new Player();
 	var p2 = new Player();
@@ -35,20 +35,24 @@ function Player(name, health, punch, kick, specialtyHit, bite) {
 	p2.name = "Zombie";
 
 	Player.prototype.heal = function (player) {
-		player.health += 10;
+		player.health += 5;
 	}
 
 
 	$("#punch").on('click', function(){
-		$("this.punch").html(alert(p1.name + " punched the crap out of " + p2.name));
+		p1.punch(p2);
+		$("#punch").html(alert(p1.name + " punched the crap out of " + p2.name + " and now has " + p2.health + " health left"));
 	});
 	$("#kick").on('click', function(){
-		$("this.kick").html(alert(p1.name + " kicked " + p2.name));
+		p1.kick(p2);
+		$("#kick").html(alert(p1.name + " dropkicked " + p2.name + " now " + p2.name + " has " + p2.health + " health left"));
 	});
 	$("#specialty").on('click', function(){
-		$("this.specialtyHit").html(alert(p1.name + " did some crazy sweet moves on " + p2.name));
+		p1.specialtyHit(p2);
+		$("this.specialtyHit").html(alert(p1.name + " did some crazy sweet moves on " + p2.name + " that caused critical damage!" + p2.name + " has " + p2.health + " health left"));
 	});
 	$("#heal").on('click', function(){
-		$("heal").html(alert(p1.name + " ate a delicious Chimichanga and now has " + p1.health + " health left"));
+		p1.heal(p1);
+		$("heal").html(alert(p1.name + " ate a delicious Chimichanga to heal himself and now has " + p1.health + " health left"));
 	});
 });
