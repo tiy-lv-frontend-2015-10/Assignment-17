ad6a04f25c11a805a08276815d1992d953efc2e0
Monica <monicamillerg@gmail.com>
2015-11-08 12:34:50 -0800

    created weapons

--- main.js
+++ main.js
@@ -1,66 +1,62 @@
-function Character () {
-  this.name;
-  this.hitpoints = 500;
-  this.energy = 400;
+function Character (name, health, weapon) {
+  this.name = name;
+  this.health = health;
+  this.weapon = weapon;
+
+  this.isAlive = function(){
+    return this.health > 0; 
+  }
+
   this.attack = function attack(opponent) {
-  opponent.hitpoints -=100
-  alert(this.name + " just attack " + opponent.name);
+    opponent.health -= 10;
+    console.log(this.name + " just attacked " + opponent.name + " for 10 points!");
+    console.log(opponent.name + "'s health is now " + opponent.health);
+    console.log("");
   };
+
+  /* we may use these in the future. Keep them for now
+  
   this.killHit = function (opponent) {
     opponent.heal -= this.randomCrit();
   };
+  
   this.randomCrit = function () {
     var max = 20;
     var min = 11;
 
     return Math.floor(Math.random() * (max - min) + min);
+
   }
+  */
 };
 
-// my players
-
-var p1 = new Character();
-var p2 = new Character();
-
-
-p1.name = "Knight"
-p2.name = "Dragon"
-
-
-p1.attack(p2);
-alert(p2.name + " has " + p2.hitpoints + " points left. The Dragon it is in bad mood !!!");
-
-p2.attack(p1);
-alert(p1.name + " has been attack")
-alert(p1.name + " has " + p1.hitpoints + " points left ");
-
-Character.prototype.heal = function heal(targetCharacter){
-  targetCharacter.hitpoints += 200;
-} 
-
-p2.heal(p1);
-alert(p1.name + " has " + p1.hitpoints + " points left after been heal ");
-
-/*p1.attack(p2);
-console.log(p2.name + " was attacked and has " + p2.health + " health left");
-
-Character.prototype.heal = function () {
- Character.health += 5;
+function Weapon(name, minAttack, maxAttack){
+  this.name = name;
+  this.minAttack = minAttack;
+  this.maxAttack = maxAttack;
 }
 
-Player.prototype.emotionalAttack = function (player) {
-  player.health -= 65;
+function declareWinner(player){
+  alert(player.name + " is the winner!!");
+ }
+
+// my players 
+var sword = new Weapon("Sword", 0, 20);
+var knight = new Character("Knight", 50, sword);
+
+var flame = new Weapon("Flame", 0, 10);
+var dragon = new Character("Dragon", 100, flame);
+
+while (knight.isAlive() && dragon.isAlive()) {
+  knight.attack(dragon);
+  if (!dragon.isAlive()){
+    declareWinner(knight);
+    break;
+  }
+
+  dragon.attack(knight);
+  if (!knight.isAlive()){
+    declareWinner(dragon);
+    break;
+  }
 }
-p1.heal(p2);
-console.log(p2.name + " was healed and has " + p2.health + " health left");
-
-p2.criticalHit(p1);
-console.log(p1.name + " was critically attacked and has " + p1.health + " health left");
-
-p1.emotionalAttack(p2);
-console.log(p1.name + " told " + p2.name + " the horrifying truth. " + p2.name + " couldn't handle the truth. NOOOOOOO!!!!");
-console.log(p2.name + " has " + p2.health + " health left. Poor guy");*/
-
-
-
-
