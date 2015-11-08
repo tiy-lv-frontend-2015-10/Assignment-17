f4d9041c489efb500e925f7613dd46a9158b358b
Domingo Rosa <DomingoRosa@192.168.1.103>
2015-11-07 19:26:37 -0800

    added sounds and health bar

--- game.js
+++ game.js
@@ -1,16 +1,27 @@
 $( document ).ready(function() {
+
+  var saber = new Audio ('sounds/lightsaberClash.mp3');
+  var ligthning = new Audio ('sounds/forceLightning.mp3');
+  var force = new Audio ('sounds/forceAwaken.mp3')
+
+
 var Character = function(name, hp, a1, sec, sp) {
   this.name = name;
-  this.hp;
+  this.hp = 100;
+  this.a1 = a1;
+  this.sec = sec;
+  this.sp = sp;
 
   
   this.attack = function attack (opponent) {
     var hitChance = Math.random();
     if (hitChance > .3 && hitChance <.7) {
       opponent.hp -= 10;
-     alert(this.name + " used " + a1 + " and it hit ");
-    } else if (hitChance >.71){
+      saber.play();
+      alert(this.name + " used " + a1 + " and it hit ");
+    } else if (hitChance >.7){
       opponent.hp -=25;
+      saber.play();
       alert(this.name + " used " + a1 + " attack and it was CRITICAL!!!!");
     }else{
       alert(this.name + " used " + a1 + " attack and missed!!!!");
@@ -40,38 +51,87 @@ var Character = function(name, hp, a1, sec, sp) {
     }
   };
 
+};
+var ObiWan  = new Character("Obi Wan", 100, "Saber Slash", "Force Push", "Force Awaken");
+var DarthVader = new Character("Darth Vader", 100, "Saber Slash", "Force Choke", "Sith Ligthning");
 
-      $('.player1 .attack').on('click', function attack (opponent) {
-    $('.battleLog').html(ObiWan.name + ' attacks with a ' + ObiWan.a1 + '. ');
-    $('.battleLog').html(DarthVader.name + " has " + DarthVader.hp + " hp left");
+
+  $('.player1 .attack').on('click', function() {
+    $('.player1 button').hide();
+    $('.player2 button').show();
+    ObiWan.attack(DarthVader);
+    $('.battle').html(ObiWan.name + ' attacks with a ' + ObiWan.sec + '. ');
+    $('#vaderHp').val(DarthVader.hp);
+    if (DarthVader.hp <= 0) {
+      alert(ObiWan.name + " is Victorious");
+      location.reload();
+    }
   });
 
-    $('.player1 .secondary').on('click', function secondary (opponent) {
-    $('.battleLog').html(ObiWan.name + ' attacks with a ' + ObiWan.sec + '. ');
+  $('.player1 .secondary').on('click', function() {
+    $('.player1 button').hide();
+    $('.player2 button').show();
+    ObiWan.secondary(DarthVader);
+    $('.battle').html(ObiWan.name + ' attacks with a ' + ObiWan.sec + '. ');
+    $('#vaderHp').val(DarthVader.hp);
+     if (DarthVader.hp <= 0) {
+      alert(ObiWan.name + " is Victorious");
+      location.reload();
+    }
   });
 
-    $('.player1 .special').on('click', function special (opponent) {
-    $('.battleLog').html(ObiWan.name + ' attacks with a ' + ObiWan.sp + '. ');
+  $('.player1 .special').on('click', function() {
+    force.play();
+    $('.player1 button').hide();
+    $('.player2 button').show();
+    ObiWan.special(DarthVader);
+    $('.battle').html(ObiWan.name + ' attacks with a ' + ObiWan.sp + '. ');
+    $('#vaderHp').val(DarthVader.hp);
+     if (DarthVader.hp <= 0) {
+      alert(ObiWan.name + " is Victorious");
+      location.reload();
+    }
   });
 
   ////////////////////////////////
 
-    $('.player2 .attack').on('click', function attack (opponent) {
-    $('.battleLog').html(DarthVader.name + ' attacks with a ' + DarthVader.a1 + '. ');
+  $('.player2 .attack').on('click', function() {
+    saber.play();
+    $('.player2 button').hide();
+    $('.player1 button').show();
+    DarthVader.attack(ObiWan)
+     $('.battle').html(DarthVader.name + ' attacks with a ' + DarthVader.sp + '. ');
+     $('#obiHp').val(ObiWan.hp);
+     if (ObiWan.hp <= 0) {
+      alert(DarthVader.name + " is Victorius");
+      location.reload();
+    }
   });
 
-    $('.player2 .secondary').on('click', function secondary (opponent) {
-    $('.battleLog').html(DarthVader.name + ' attacks with a ' + DarthVader.sec + '. ');
+  $('.player2 .secondary').on('click', function() {
+    $('.player2 button').hide();
+    $('.player1 button').show();
+    DarthVader.secondary(ObiWan)
+     $('.battle').html(DarthVader.name + ' attacks with a ' + DarthVader.sp + '. ');
+     $('#obiHp').val(ObiWan.hp);
+     if (ObiWan.hp <= 0) {
+      alert(DarthVader.name + " is Victorius");
+      location.reload();
+  }
   });
 
-    $('.player2 .special').on('click', function special (opponent) {
-    $('.battleLog').html(DarthVader.name + ' attacks with a ' + DarthVader.sp + '. ');
+  $('.player2 .special').on('click', function() {
+    ligthning.play();
+    $('.player2 button').hide();
+    $('.player1 button').show();
+    DarthVader.special(ObiWan)
+     $('.battle').html(DarthVader.name + ' attacks with a ' + DarthVader.sp + '. ');
+     $('#obiHp').val(ObiWan.hp);
+     if (ObiWan.hp <= 0) {
+      alert(DarthVader.name + " is Victorius");
+      location.reload();
+  }
   });
-
-
-var ObiWan  = new Character("Obi Wan", 100, "Sabre Slash", "Force Push", "Force Awaken");
-var DarthVader = new Character("Darth Vader", 100, "Sabre Slash", "Force Choke", "Sith Ligthning");
-};
 
 });
 
